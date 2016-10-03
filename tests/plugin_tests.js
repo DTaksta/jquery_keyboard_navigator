4c8c3231b66c596338239774b5c868902a97e36f
DTaksta <deontakpuie@gmail.com>
2016-10-03 04:47:25 +0200

    Add files via upload

---
+++ tests/plugin_tests.js
@@ -0,0 +1,18 @@
+var chai = require('chai');
+var  = require('myKeyboardNavigatorPlugin.js');
+var expect = chai.expect;
+
+describe('KeyBoardPlugin Tests', function() {
+	
+});
+
+/*
+var event = jQuery.Event("keyup");
+event.keyCode = 72;
+$(".selector").trigger(event);
+*/
+/*
+expect($("<div><span>foo</span> bar</div>"))
+  .to.have.$text("foo bar").and
+  .to.contain.$text("foo");
+*/
